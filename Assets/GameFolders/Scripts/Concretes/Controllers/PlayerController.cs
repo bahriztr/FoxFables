ac1c594e2fb787bbc50b9fd0b9439da125478803
bahriztr <bahriztr3432@gmail.com>
2024-02-20 23:39:03 +0300

    +Player run task done

--- Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -7,16 +7,37 @@ namespace FoxFables.Controllers
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private float speed;
+        private Animator _anim;
+        private float _horizontal;
+
+        private void Awake()
+        {
+            _anim = GetComponent<Animator>();
+        }
 
         private void Update()
+        {
+            InputProcess();
+        }
+
+        private void FixedUpdate()
         {
             PlayerMovement();
         }
 
+        private void InputProcess()
+        {
+            _horizontal = Input.GetAxis("Horizontal");
+        }
+
         private void PlayerMovement()
         {
-            float horizontal = Input.GetAxis("Horizontal");
-            transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+
+            _anim.SetFloat("moveSpeed", Mathf.Abs(_horizontal));
+            transform.Translate(Vector2.right * _horizontal * Time.deltaTime * speed);
+
+            if(_horizontal != 0)
+                transform.localScale = new Vector2(Mathf.Sign(_horizontal), 1);
         }
     }
 }
