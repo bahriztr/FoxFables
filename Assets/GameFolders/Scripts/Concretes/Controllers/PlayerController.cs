5cc65462798db6f054f39681fb3fa75d18cc4fc2
bahriztr <bahriztr3432@gmail.com>
2024-02-22 01:02:17 +0300

    +Player controller seperated with all actions

--- Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -1,33 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FoxFables.Inputs;
+using FoxFables.Abstracts.Inputs;
+using FoxFables.Movements;
+using FoxFables.Animation;
 
 namespace FoxFables.Controllers
 {
     public class PlayerController : MonoBehaviour
     {
-        [Header("Force Values")]
-        [SerializeField] private float speed;
-        [SerializeField] private float _jumpForce;
-
         [Header("Directional Values")]
         private float _horizontal;
-
-        [Header("Animation")]
-        private Animator _anim;
-
-        [Header("Components In Hierarchy")]
-        private Rigidbody2D _rb;
 
         [Header("Booleans")]
         private bool _isJump;
 
-        public bool IsJumpAction => _rb.velocity != Vector2.zero;
+        [Header("Instance Objects")]
+        private IPlayerInput _input;
+        private Mover _mover;
+        private Jump _jump;
+        private CharacterAnimation _characterAnimation;
+        private Flip _flip;
+
 
         private void Awake()
         {
-            _anim = GetComponent<Animator>();
-            _rb = GetComponent<Rigidbody2D>();
+            _characterAnimation = GetComponent<CharacterAnimation>();
+            _input = new PcInput();
+            _mover = GetComponent<Mover>();
+            _jump = GetComponent<Jump>();
+            _flip = GetComponent<Flip>();
         }
 
         private void Update()
@@ -43,9 +46,9 @@ namespace FoxFables.Controllers
 
         private void InputProcess()
         {
-            _horizontal = Input.GetAxis("Horizontal");
+            _horizontal = _input.Horizontal;
 
-            if (Input.GetButtonDown("Jump"))
+            if (_input.IsJumpButtonDown)
             {
                 _isJump = true;
             }
@@ -53,22 +56,20 @@ namespace FoxFables.Controllers
 
         private void PlayerMovement()
         {
-            _anim.SetFloat("moveSpeed", Mathf.Abs(_horizontal));
-            transform.Translate(Vector2.right * _horizontal * Time.deltaTime * speed);
-
-            if(_horizontal != 0)
-                transform.localScale = new Vector2(Mathf.Sign(_horizontal), 1);
+            _mover.HorizontalMovement(_horizontal);
+            _characterAnimation.MoveAnimation(_horizontal);
+            _characterAnimation.JumpAnimation(_jump.IsJumpAction);
+            _flip.FlipCharacter(_horizontal);
         }
 
         private void PlayerJump()
         {
             if(_isJump)
             {
-                _rb.AddForce(Vector2.up * _jumpForce);
+                _jump.JumpAction();
                 _isJump = false;
             }
 
-            _anim.SetBool("isJump", IsJumpAction);
         }
     }
 }
